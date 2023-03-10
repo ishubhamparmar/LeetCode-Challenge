05d9322a677715f70c9b2c2ec6483ea2c2329fa3
Shubham Parmar <shubhamparmarmoney@gmail.com>
2023-03-10 17:32:59 +0530

    Added 121. Best Time to Buy and Sell Stock

---
+++ 121_Best_Time.java
@@ -0,0 +1,70 @@
+/*
+ * 121. Best Time to Buy and Sell Stock
+ * Problem Link:https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * 
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the ith day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction. If you
+ * cannot achieve any profit, return 0.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+ * 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you
+ * must buy before you sell.
+ */
+
+//Brute Force O(n2) - TLE
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int maxPro = 0;
+//         for(int i=0; i<prices.length; i++){
+//             for(int j = i; j<prices.length; j++){
+//                 maxPro = Math.max(maxPro, prices[j]-prices[i]);
+//             }
+//         }
+//         return maxPro;
+//     }
+// }
+
+//Optimized - NoTLE - O(N)
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int [] aux = new int [prices.length];
+//         int maxPro = 0;
+//         aux [prices.length-1] = prices [prices.length-1];
+//         for(int i=prices.length-2; i>=0; i--){
+//             if(aux[i+1] < prices[i])
+//                 aux[i] = prices[i];
+//             else
+//                 aux [i] = aux [i+1];
+//         }
+//         for(int i=0; i<prices.length; i++){
+//             maxPro = Math.max(maxPro, aux[i]-prices[i]);
+//         }
+//         return maxPro;
+//     }
+// }
+
+//Constant Time - O(1) and Constant Space Complexity O(1)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minSoFar = prices[0];
+        int maxPro = 0;
+        for (int i = 0; i < prices.length; i++) {
+            minSoFar = Math.min(minSoFar, prices[i]);
+            maxPro = Math.max(maxPro, prices[i] - minSoFar);
+        }
+
+        return maxPro;
+    }
+}
