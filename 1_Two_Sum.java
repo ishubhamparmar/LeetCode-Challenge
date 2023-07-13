06af290c9d3362d219a2e66427ec29ef09b8f10e
Shubham Parmar <shubhamparmarmoney@gmail.com>
2023-07-13 20:15:25 +0530

    Updated optimized solution for 1. Two Sum

--- 1_Two_Sum.java
+++ 1_Two_Sum.java
@@ -1,36 +1,51 @@
 /*
+ * 
+ * 1. Two Sum
+ * Problem Link: https://leetcode.com/problems/two-sum/
+ * 
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ */
+// Brute force | O(N2)
+// class Solution {
+// public int[] twoSum(int[] nums, int target) {
+// int [] arr = new int [2];
+// for(int i=0; i<nums.length; i++){
+// for(int j=1; j<nums.length; j++){
+// if(nums[i] + nums[j] == target && i!=j){
+// arr[0] = i;
+// arr[1] = j;
+// }
+// }
+// }
+// return arr;
+// }
 
-1. Two Sum
-Problem Link: https://leetcode.com/problems/two-sum/
+// }
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
-
-Example 1:
-
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-*/
-
-
-
+// Using HashMap | O(NlogN)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] arr = new int [2];
-        for(int i=0; i<nums.length; i++){
-            for(int j=1; j<nums.length; j++){
-                if(nums[i] + nums[j] == target && i!=j){
-                    arr[0] = i;
-                    arr[1] = j;
-                }
+        // int [] arr = new int [2];
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
+                return new int[] { hm.get(target - nums[i]), i };
             }
+            hm.put(nums[i], i);
         }
-    return arr;
+        return new int[] {};
     }
-    
 }
