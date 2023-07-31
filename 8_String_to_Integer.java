75458d80e1f8079255a171328bb240a81f076d25
Shubham Parmar <shubhamparmarmoney@gmail.com>
2023-07-31 22:09:51 +0530

    Update 8. String to Integer(atoi)

--- 8_String_to_Integer.java
+++ 8_String_to_Integer.java
@@ -1,6 +1,6 @@
 /*
  * 
- * 8. String to Integer (atoi)
+ * 8. String to Integer (atoi) - Medium
  * Problem Link: https://leetcode.com/problems/string-to-integer-atoi/
  * Implement the myAtoi(string s) function, which converts a string to a 32-bit
  * signed integer (similar to C/C++'s atoi function).
@@ -45,6 +45,8 @@
  * Since 42 is in the range [-231, 231 - 1], the final result is 42.
  * 
  */
+
+//Beats 61%
 class Solution {
     public int myAtoi(String s) {
         if (s == null)
