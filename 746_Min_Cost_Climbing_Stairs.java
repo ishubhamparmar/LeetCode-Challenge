4ffcba6733f76617c6ad23f47780f409da122f59
Shubham Parmar <shubhamparmarmoney@gmail.com>
2023-08-07 10:25:17 +0530

    Update 746. Min Cost Climbing Stairs

--- 746_Min_Cost_Climbing_Stairs.java
+++ 746_Min_Cost_Climbing_Stairs.java
@@ -56,26 +56,47 @@
 // }
 
 //DP | Beats 56%
+// class Solution {
+//         public int minCostClimbingStairs(int[] cost) {
+//                 int n = cost.length;
+//                 int[] dp = new int[n + 1];
+//                 Arrays.fill(dp, -1);
+//                 dp[n] = Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+//                 return dp[n];
+//         }
+
+//         public int solve(int cost[], int n, int[] dp) {
+//                 if (n == 0) {
+//                         return cost[0];
+//                 }
+//                 if (n == 1) {
+//                         return cost[1];
+//                 }
+
+//                 if (dp[n] != -1)
+//                         return dp[n];
+//                 dp[n] = cost[n] + Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+//                 return dp[n];
+//         }
+// }
+
+//DP | Tabulation | Beats 56%
 class Solution {
-        public int minCostClimbingStairs(int[] cost) {
-                int n = cost.length;
-                int[] dp = new int[n + 1];
-                Arrays.fill(dp, -1);
-                dp[n] = Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
-                return dp[n];
-        }
+    public int minCostClimbingStairs(int[] cost) {
+        int n =  cost.length;
+        return solve(cost, n);
+    }
 
-        public int solve(int cost[], int n, int[] dp) {
-                if (n == 0) {
-                        return cost[0];
-                }
-                if (n == 1) {
-                        return cost[1];
-                }
-
-                if (dp[n] != -1)
-                        return dp[n];
-                dp[n] = cost[n] + Math.min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
-                return dp[n];
+    public int solve(int cost[], int n) {
+        //Step-1: Create dp array
+        int [] dp = new int [n+1];
+        //Step-2: Base case initialize
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        //Step-3
+        for(int i=2; i<n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
         }
+        return Math.min(dp[n-1], dp[n-2]);
+    }
 }
